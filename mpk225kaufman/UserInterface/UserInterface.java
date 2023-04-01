28cbb01e4517b8d2181f0489960191f9f4082836
mikeKaufman310 <mpk225@lehigh.edu>
2023-04-01 13:55:11 -0400

    function definitons and implementations 4/1

--- mpk225kaufman/UserInterface/UserInterface.java
+++ mpk225kaufman/UserInterface/UserInterface.java
@@ -9,6 +9,7 @@ public class UserInterface{
     //Fields
     static String password = "";
     static String userName = "";
+    static Scanner scan = new Scanner(System.in);
 
 
     /**
@@ -18,13 +19,16 @@ public class UserInterface{
     public static void main(String[] args){
         //display logo
         displayLogo();
-        //NOTE: maybe loop  login attempts
-        logIn();
+        //NOTE: maybe loop  login attempts and handle wrong password
+        logIn(scan);
+        System.out.println();
 
         
         try( Connection con = DriverManager.getConnection("jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241",userName,password);
             Statement s = con.createStatement();){
                 displayMenu();
+                int choice = menuOption(scan);
+                runOption(choice);
         }
         catch(Exception e){
             System.out.println(e);      //NOTE: make good Exception Catch
@@ -57,11 +61,10 @@ public class UserInterface{
         return dates;
     }
 
-    public static void logIn(){
+    public static void logIn(Scanner scan){
         System.out.println("Login:");
         System.out.println("--------------------");
         boolean go = false;//loop control variable
-        Scanner scan = new Scanner(System.in);
         while(!go){
             try{
                 System.out.print("Input Username:\t");
@@ -92,5 +95,70 @@ public class UserInterface{
     //returns true if possible sql injection attack, false otherwise
     public static boolean sqlInjection(String userName){
         return false;//set false for testing before implementation
+    }
+
+    public static int menuOption(Scanner scan){
+        int choice = -1;//-1 if no choice made
+        boolean go = false;//loop control boolean
+        System.out.println();
+        do{
+            try{
+                System.out.print("Enter a Choice (1-6):\t");
+                //scan.next();
+                String tempChoiceString = scan.next();
+                int tempChoice = Integer.parseInt(tempChoiceString);
+                if(tempChoice >= 1 && tempChoice <= 6){
+                    choice = tempChoice;
+                    go = true;
+                }else{
+                    throw new Exception();
+                }
+            }
+            catch(Exception e){
+                System.out.println("Invalid Input, Try Again");
+            }
+        }while(!go);
+        scan.close();
+        return choice;
+    }
+
+    public static void runOption(int choice){
+        if(choice == 1){
+            makeReservation();
+        }else if(choice == 2){
+            checkIn();
+        }else if(choice == 3){
+            checkOut();
+        }else if(choice == 4){
+            cancellation();
+        }else if(choice == 5){
+            payment();
+        }else{
+            seeAvailablity();
+        }
+    }
+
+    public static void makeReservation(){//need arguments
+        
+    }
+
+    public static void checkIn(){//need argument
+    
+    }
+
+    public static void checkOut(){//need arguments
+    
+    }
+
+    public static void cancellation(){//need argument
+    
+    }
+
+    public static void payment(){//need arguments
+    
+    }
+
+    public static void seeAvailablity(){//need arguments
+    
     }
 }
